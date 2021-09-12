311b724783cb48115cccd189657c65c8ef18f5af
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-09-12 13:45:40 -0500

    -updated accessibility issue

--- src/components/country/CountryCard.js
+++ src/components/country/CountryCard.js
@@ -11,7 +11,7 @@ const CountryCard = ({ country }) => {
                     <img src={flag} alt="flag" />
                 </div>
                 <div className='country-card-info'>
-                    <h3 className="country-card-info-title">{name}</h3>
+                    <h2 className="country-card-info-title">{name}</h2>
                     <div className="country-card-info-stats"><span className="bold">Population: </span><span>{population.toLocaleString()}</span></div>
                     <div className="country-card-info-stats"><span className="bold">Region: </span><span>{region}</span></div>
                     <div className="country-card-info-stats"><span className="bold">Capital: </span><span>{capital}</span></div>
